b95bb443cefced57c1cc5b3e2bb49da36471b545
andry-titoruk <51340921+andry-titoruk@users.noreply.github.com>
2019-06-14 13:33:26 +0300

    Add files via upload

---
+++ ChristmasTree.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	//number of a string
+	int i = 0;
+	int a = 0;
+	int enteredNum;
+
+	cout << "Enter the number:";
+	cin >> enteredNum;
+
+	
+	
+    
+	while (i < enteredNum - i)
+	{
+		int gap = 0;
+		int gapsQuantity = ((enteredNum - 1) / 2) - i;
+
+		while (gap < gapsQuantity)
+		{
+			cout << " ";
+			gap++;
+		}
+		//number of stars have to be in the string
+		
+		while (a < i + 1)
+		{
+			cout << "*";
+			a++;
+		}
+		cout << "\n";
+		i++;
+		a = a - 2 * i;
+		/*gapsQuantity = gapsQuantity - i;*/
+	}
+	cout << "\n";
+	return 0;
+}
