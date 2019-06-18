b6edc3ddfb1e88e92a8e361d8c7e384111a6c4d8
andry-titoruk <51340921+andry-titoruk@users.noreply.github.com>
2019-06-18 16:26:45 +0300

    Add files via upload

---
+++ countingOfBits.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int enteredNum;
+	//the remainder of %-division
+	int a;
+	//the quantity of '1' in the string
+	int i = 0;
+
+	cout << "Enter the number:" << endl;
+	cin >> enteredNum;
+
+	while (enteredNum > 0 && enteredNum < 4294967296)
+	{
+		a = enteredNum % 2;
+
+		if (a == 0)
+		{
+			cout << '0';
+			enteredNum = enteredNum / 2;
+		}
+		else if (a == 1)
+		{
+			cout << '1';
+			enteredNum = enteredNum / 2;
+			i++;
+		}
+	}
+	cout << "\nThe quantity of '1' in the string is: "; cout <<i;
+	return 0;
+}
---
+++ summAndAve.cpp
@@ -0,0 +1,21 @@
+﻿#include <cstdio>
+
+int main(int argc, char const *argv[])
+{
+	long long i = 0;
+	unsigned number = 12345;
+	printf("Summ of the number %d ====>", number);
+	long long summ = 0;
+	while (number != 0 )
+	{
+		summ += number % 10;
+		number /= 10;
+		i++;
+	}
+	printf("%lld\n", summ);
+	
+	auto calcSumm = summ / i;
+	printf("%lld", calcSumm);
+	/*sprintf(/*summ, " / ", i, " = ", summ / i)*/
+	return 0;
+}
